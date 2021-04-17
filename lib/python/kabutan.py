357bb001e2dfb0f5a3e294595d662a8dd7e5a2c7
yuusaiz <yuusaiz@gmail.com>
2021-04-18 01:55:18 +0900

    classに。

--- lib/python/kabutan.py
+++ lib/python/kabutan.py
@@ -1,29 +1,40 @@
-def yu_login_kabutan(uname, passwd):
-  session = requests.session()
-  url="https://account.kabutan.jp/login"
-  #html = urllib.request.urlopen(url).read()
-  res = session.get(url)
-  html = res.content
-  soup = BeautifulSoup(html,"html.parser")
-  input_email = soup.select("#session_email")
-  input_passwd = soup.select("#session_password")
-  authenticity_token = soup.find('input', attrs={'name':'authenticity_token', 'type':'hidden'})
-  #print("TOKEN")
-  #print(authenticity_token['value'])
-  #print("TOKEN end")
-  # ログイン
-  login_info = {
-    "session[email]":uname,
-    "session[password]":passwd,
-    "authenticity_token":authenticity_token['value'],
-    "session[remember_me]":"0"
-  }
-  # action
-  url_login = url
-  res = session.post(url_login, data=login_info)
-  res.raise_for_status()
-  #print(res.text)
-  if "/logout" in res.text:
-    #print("ログイン成功")
-    return True
-  return False
+import yu_web 
+
+class yu_kabutan(YuWeb):
+  def __init__(self):
+    session = requests.session()
+
+  def yu_login_kabutan(self, uname, passwd):
+    #session = requests.session()
+    url="https://account.kabutan.jp/login"
+    #html = urllib.request.urlopen(url).read()
+    res = session.get(url)
+    html = res.content
+    soup = beautifulsoup(html,"html.parser")
+    input_email = soup.select("#session_email")
+    input_passwd = soup.select("#session_password")
+    authenticity_token = soup.find('input', attrs={'name':'authenticity_token', 'type':'hidden'})
+    #print("token")
+    #print(authenticity_token['value'])
+    #print("token end")
+    # 繝ｭ繧ｰ繧､繝ｳ
+    login_info = {
+      "session[email]":uname,
+      "session[password]":passwd,
+      "authenticity_token":authenticity_token['value'],
+      "session[remember_me]":"0"
+    }
+    # action
+    url_login = url
+    res = session.post(url_login, data=login_info)
+    res.raise_for_status()
+    #print(res.text)
+    if "/logout" in res.text:
+      #print("繝ｭ繧ｰ繧､繝ｳ謌仙粥")
+      return true
+    return false
+
+
+if __name__ == "__main__":
+  yu = YuKabutan()
+
