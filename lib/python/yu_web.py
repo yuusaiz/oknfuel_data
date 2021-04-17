0b43b87ae16227218c234327e9da121149734fe6
yuusaiz <yuusaiz@gmail.com>
2021-04-18 02:36:49 +0900

    yu_web

---
+++ lib/python/yu_web.py
@@ -0,0 +1,13 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import sys
+import math
+import requests
+
+
+class YuWeb():
+  session
+  def __init__(self):
+    session = requests.session()
+
+
