fdfb1d37b2c6934338cca8584872ed711c3b76fb
arle <arle@DESKTOP-MTLKALL.localdomain>
2021-04-19 22:50:07 +0900

    add test

--- lib/python/yu_kabutan.py
+++ lib/python/yu_kabutan.py
@@ -13,6 +13,7 @@ import sys
 import math
 import requests
 import yu_web 
+import unittest
 
 class yu_kabutan(yu_web.yu_web):
   def __init__(self):
@@ -57,17 +58,40 @@ class yu_kabutan(yu_web.yu_web):
   def get_quarter_settlement(self):
     self.quarter_settlement = {}
     self.quarter_settlement['keijo'] = [] 
+    self.quarter_settlement['uriage'] = [] 
+    self.quarter_settlement['eigyo'] = [] 
+    self.quarter_settlement['saishu'] = [] 
+    self.quarter_settlement['hitokabueki'] = [] 
+    self.quarter_settlement['haito'] = [] 
     #self.quarter_settlement['keijo'] = pd.Series()
     divs = self.soup.find('div',{'class':'fin_q_t0_d fin_q_t1_d'})
     for trs in divs.find_all("tr"):
       tds = trs.find_all("td")
       if len(tds)==7 and ('/' in tds[6].text):
-        self.quarter_settlement['keijo'].append(int(tds[2].text.replace(',', '')))
+        self.quarter_settlement['uriage'].append(float(tds[0].text.replace(',', '')))
+        self.quarter_settlement['eigyo'].append(yu_web.util.try_float(tds[1].text.replace(',', '')))
+        self.quarter_settlement['keijo'].append(float(tds[2].text.replace(',', '')))
+        self.quarter_settlement['saishu'].append(float(tds[3].text.replace(',', '')))
+        self.quarter_settlement['hitokabueki'].append(float(tds[4].text.replace(',', '')))
+        self.quarter_settlement['haito'].append(float(tds[5].text.replace(',', '')))
 
+
+class yu_kabutan_test(unittest.TestCase):
+  def test1(self):
+    self.yu = yu_kabutan()
+    f = open('u.txt', 'r', encoding="ascii")
+    u = f.read().replace('\n','')
+    f.close()
+    f = open('p.txt', 'r', encoding="ascii")
+    p = f.read().replace('\n','')
+    f.close()
+    print(u)
+    print(p)
+    self.assertEqual(True, self.yu.login_kabutan(u, p))
+
+    self.yu.set_target_code("9984")
+    self.yu.get_quarter_settlement()
+    self.assertEqual(requests.sessions.Session, type(yu.session))
 
 if __name__ == "__main__":
-  yu = yu_kabutan()
-  yu.login_kabutan(sys.argv[1],sys.argv[2])
-  yu.set_target_code("9984")
-  yu.get_quarter_settlement()
-  print(yu.quarter_settlement['keijo'])
+  unittest.main()
--- lib/python/yu_web.py
+++ lib/python/yu_web.py
@@ -3,7 +3,15 @@ from bs4 import BeautifulSoup
 import sys
 import math
 import requests
+import unittest
 
+class util:
+  def try_float(f):
+    try:
+      return(float(f))
+    except:
+      return(None)
+          
 
 class yu_web:
   def __init__(self):
@@ -12,5 +20,19 @@ class yu_web:
     self.soup = None
 
 
+class yu_web_test(unittest.TestCase):
+  def test1(self):
+    yu = yu_web()
+    self.assertEqual(requests.sessions.Session, type(yu.session))
+
+  def test_try_float(self):
+    self.assertEqual(123, util.try_float("123"))
+    self.assertEqual(123, util.try_float("123.0"))
+    self.assertEqual(123.1, util.try_float("123.1"))
+    self.assertEqual(None, util.try_float("-"))
+    self.assertEqual(None, util.try_float("－"))
+    self.assertEqual(-123, util.try_float("-123"))
+
+
 if __name__ == "__main__":
-  yu = yu_web()
+  unittest.main()
